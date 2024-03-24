0bf33de4f653983f91614256830f03f6c34548f4
joel langat <langatjoel42@gmail.com>
2024-03-24 10:29:21 +0300

    made some functions in index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = ["surprise"];
+
+function writeCards(names, event){
+    let thankCard =[]
+   for (let i = 0; i < names.length; i++ ){
+    thankCard.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+   } 
+return thankCard
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number -= 1;
+    }
+
+}
+
